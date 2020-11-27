ffead47f0b2b024f076b806c6e740e23bdd144be
Lukas Siegele <lukas.siegele@gmx.de>
2020-11-27 16:12:18 +0100

    Function Components, Styled Components, StaticJSON

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,50 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useEffect } from 'react'
+import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = ({ siteTitle }) => {
+  // State definieren
+  const [hasScrolled, setHasScrolled] = useState(false);
+  // hasScrolled = lesen, variable; setHasScrolled = schreiben, funktion
+
+  // 1x mal eventlistener global hinzufügen - bei jeder scrollbewegung checken
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => { // wird ausgeführt wenn die funktion unmounted wird
+       window.removeEventListener('scroll', handleScroll)
+    }
+  }, [] //updated sich wenn neue daten reinkommen, bei leer wirds 1x mal beim mounten ausgeführt (compoenntdidmount)
+  )
+
+  // ändern des states bei über 50 offset
+  const handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      setHasScrolled(true);
+    } else {
+      setHasScrolled(false);
+    }
+  }
+
+  return (
+    <div className={hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+      <div className="HeaderGroup">
+        <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/Buy"><button>Buy</button></Link>
+      </div>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+
+// const functionABC = (parameter) => {
+//   return 3;
+// }
+
+// const functionABC = (parameter) => (3)
 
 export default Header
